9cc77eec879deeb4fbf3de2d19fc7aaab38be8ce
OleksiiHordiienko82 <79364122+OleksiiHordiienko82@users.noreply.github.com>
2021-06-11 01:12:11 +0300

    task24

--- src/index.js
+++ src/index.js
@@ -2116,5 +2116,70 @@ for(const product of products ) {
     return 0;  
 
   // Пиши код выше этой строки
-}
+  }
+//   Задание22
+//   В прогнозе максимальных температур также может
+//   быть необязательное свойство icon - иконка погоды.
+//   Замени объявления переменных yesterday, today,
+//     tomorrow и icon одной операцией деструктуризации
+//   свойств объекта highTemperatures.Задай значение по
+//   умолчанию для icon - строку "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+// Тесты
+// Объявлена переменная highTemperatures
+// Значение переменной highTemperatures это объект
+// Объявлена переменная highTemperatures
+// Значение переменной highTemperatures это объект
+// Объявлена переменная yesterday с помощью деструктуризации
+// Значение переменной yesterday это число 28
+// Объявлена переменная today с помощью деструктуризации
+// Значение переменной today это число 26
+// Объявлена переменная tomorrow с помощью деструктуризации
+// Значение переменной tomorrow это число 33
+// Объявлена переменная icon с помощью деструктуризации
+// Значение переменной icon это строка "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+// Используется деструктуризация объекта
+  const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Change code below this line
+
+const {yesterday, today, tomorrow, icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"} = highTemperatures
+
+
+// Change code above this line
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+  
+//   Задание23
+//   Замени объявления переменных highYesterday,
+//     highToday, highTomorrow и highIcon одной
+//   операцией деструктуризации свойств объекта highTemperatures.
+//   Задай значение по умолчанию для highIcon - строку
+//   "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+
+// Тесты
+// Объявлена переменная highTemperatures
+// Значение переменной highTemperatures это объект
+// Объявлена переменная highYesterday
+// Значение переменной highYesterday это число 28
+// Объявлена переменная highToday
+// Значение переменной highToday это число 26
+// Объявлена переменная highTomorrow
+// Значение переменной highTomorrow это число 33
+// Объявлена переменная highIcon
+// Значение переменной highIcon это строка "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"
+// Используется деструктуризация объекта
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Change code below this line
+
+const {yesterday: highYesterday, today: highToday, tomorrow: highTomorrow, icon: highIcon ="https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"} = highTemperatures 
+
+// Change code above this line
+const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
 

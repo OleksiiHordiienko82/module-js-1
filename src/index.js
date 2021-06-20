86df05fe0f948bb6713b687782df67718a0c198a
OleksiiHordiienko82 <79364122+OleksiiHordiienko82@users.noreply.github.com>
2021-06-20 20:48:19 +0300

    end mod 3

--- src/index.js
+++ src/index.js
@@ -2618,4 +2618,131 @@ const atTheOldToad = {
         
   },
 };
+// Задание39
+// Дополни метод removePotion(potionName) так, чтобы он удалял
+//  зелье potionName из массива зелий в свойстве potions.
 
+// Тесты
+// Объявлена переменная atTheOldToad
+// Значение переменной atTheOldToad это объект
+// Значение свойства atTheOldToad.potions это массив 
+// ["Speed potion", "Dragon breath", "Stone skin"]
+// Значение свойства atTheOldToad.removePotion это метод объекта
+// После первого вызова метода atTheOldToad.removePotion("Dragon breath"),
+//   в свойстве potions будет массив ["Speed potion", Stone skin"]
+// После второго вызова метода atTheOldToad.removePotion("Speed potion"),
+//   в свойстве potions будет массив ["Stone skin"]
+  const atTheOldToad = {
+  potions: ["Speed potion", "Dragon breath", "Stone skin"],
+  removePotion(potionName) {
+    // Change code below this line
+  const inx = this.potions.indexOf(potionName);
+    this.potions.splice(inx,1);
+    // Change code above this line
+  },
+  };
+  
+//   Задание40
+// Дополни метод updatePotionName(oldName, newName)
+//  так, чтобы он обновлял название зелья с oldName на newName, в массиве зелий в свойстве potions.
+
+// Тесты
+// Объявлена переменная atTheOldToad
+// Значение переменной atTheOldToad это объект
+// Значение свойства atTheOldToad.potions это массив 
+// ["Speed potion", "Dragon breath", "Stone skin"]
+// Значение свойства atTheOldToad.updatePotionName это метод объекта
+// После первого вызова метода atTheOldToad.updatePotionName
+//     ("Dragon breath", "Polymorth"), в свойстве potions будет массив
+//      ["Speed potion", "Polymorth", "Stone skin"]
+// После второго вызова метода atTheOldToad.updatePotionName
+//     ("Stone skin", "Invisibility"), в свойстве potions будет массив 
+//     `["Speed potion", "Polymorth", "Invisibility"]``
+  const atTheOldToad = {
+  potions: ['Speed potion', 'Dragon breath', 'Stone skin'],
+  updatePotionName(oldName, newName) {
+    // Change code below this line
+    const inx = this.potions.indexOf(oldName); 
+    this.potions.splice(inx,1,newName);
+ 
+    // Change code above this line
+  },
+  };
+  
+
+
+//   Задание41
+// Заказчица хочет чтобы каждое зелье было представлено не только именем, но и ценой, а в будущем может быть и другими характеристиками. Поэтому теперь в свойстве potions будет храниться массив объектов со следующими свойствами.
+
+// {
+//   name: "Dragon breath",
+//   price: 700
+// }
+// Выполни рефакторинг методов объекта atTheOldToad так, чтобы они работали не с массивом строк, а с массивом объектов.
+
+// getPotions() - метод для получения всех зелий. Возвращает значение свойства potions.
+// addPotion(newPotion) - добавляет зелье newPotion (уже объект) в массив в свойстве potions.
+// removePotion(potionName) - удаляет объект зелья с именем potionName из массива в свойстве potions.
+// updatePotionName(oldName, newName) - обновляет свойство name объекта-зелья с названием oldName на newName в массиве potions.
+// Тесты
+// Объявлена переменная atTheOldToad
+// Значение переменной atTheOldToad это объект
+// Значение свойства atTheOldToad.getPotions это метод объекта
+// Вызов метода atTheOldToad.getPotions() для исходного объекта возвращает [ { name: "Speed potion", price: 460 }, { name: "Dragon breath", price: 780 }, { name: "Stone skin", price: 520 } ]
+// Значение свойства atTheOldToad.addPotion это метод объекта.
+// Для исходного объекта после вызова метода atTheOldToad.addPotion({ name: "Invisibility", price: 620 }), в массиве potions последним элементом будет этот объект
+// Для исходного объекта после вызова метода atTheOldToad.addPotion({ name: "Power potion", price: 270 }), в массиве potions последним элементом будет этот объект
+// Значение свойства atTheOldToad.removePotion это метод объекта
+// Для исходного объекта после вызова метода atTheOldToad.removePotion("Dragon breath"), в свойстве potions будет массив [ { name: "Speed potion", price: 460 }, { name: "Stone skin", price: 520 } ]
+// Для исходного объекта после вызова метода atTheOldToad.removePotion("Speed potion"), в свойстве potions будет массив [ { name: "Dragon breath", price: 780 }, { name: "Stone skin", price: 520 }]
+// Значение свойства atTheOldToad.updatePotionName это метод объекта
+// Для исходного объекта после вызова метода atTheOldToad.updatePotionName("Dragon breath", "Polymorth"), в свойстве potions будет массив [{ name: "Speed potion", price: 460 }, { name: "Polymorth", price: 780 }, { name: "Stone skin", price: 520 } ]
+// Для исходного объекта после вызова метода atTheOldToad.updatePotionName("Stone skin", "Invulnerability potion"), в свойстве potions будет массив [{ name: "Speed potion", price: 460 }, { name: "Dragon breath", price: 780 }, { name: "Invulnerability potion", price: 520 } ]
+const atTheOldToad = {
+  potions: [
+    { name: 'Speed potion', price: 460 },
+    { name: 'Dragon breath', price: 780 },
+    { name: 'Stone skin', price: 520 },
+  ],
+  // Change code below this line
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(potionName) {
+  
+    this.potions.push(potionName);
+  },
+  removePotion(potionName) {
+    
+   for(let i = 0; i<this.potions.length; i+=1){
+   
+     if(this.potions[i].name === potionName){
+   
+   this.potions.splice(i,1)
+   
+   
+   }
+   
+   
+   }
+    
+    
+    
+    
+    
+    
+   
+  },
+  updatePotionName(oldName, newName) {
+    for(const potion of this.potions){
+    if( oldName === potion.name){
+    
+    potion.name = newName;
+    
+    
+    }
+    
+    }
+  },
+  // Change code above this line
+};

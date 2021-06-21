0181b17b3f87ad81d8809770672d875177a254d7
OleksiiHordiienko82 <79364122+OleksiiHordiienko82@users.noreply.github.com>
2021-06-21 16:36:27 +0300

    mod 5 task 6

--- src/index.js
+++ src/index.js
@@ -3119,3 +3119,215 @@ console.log(secondInvoke);
 // Почта poly@hotmail.de удалена из рассылки.
 console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
 
+// Модуль 5
+// Задание 1
+// Измени код так, чтобы объект parent стал прототипом для
+// объекта в переменной сhild.
+// Тесты
+// Объявлена переменная parent.
+// Значение переменной parent это объект.
+// Вызов parent.hasOwnProperty('surname') возвращает true.
+// Вызов parent.hasOwnProperty('heritage') возвращает true.
+// Объявлена переменная child.
+// Значение переменной child это объект.
+// Вызов child.hasOwnProperty('name') возвращает true.
+// Обращение к child.name возвращает 'Jason'.
+// Вызов child.hasOwnProperty('age') возвращает true.
+// Обращение к child.age возвращает 27.
+// Вызов child.hasOwnProperty('surname') возвращает false.
+// Обращение к child.surname возвращает 'Moore'.
+// Вызов child.hasOwnProperty('heritage') возвращает false.
+// Обращение к child.heritage возвращает 'Irish'.
+// Вызов parent.isPrototypeOf(child) возвращает true.
+// Используется метод Object.create().
+const parent = {
+   name: 'Stacey',
+   surname: 'Moore',
+   age: 54,
+   heritage: 'Irish'
+ };
+ // Пиши код ниже этой строки
+const child = Object.create(parent);
+ //const child = {} - было изначально
+ // Пиши код выше этой строки
+ child.name = 'Jason';
+ child.age = 27;
+
+// Задание 2
+// Измени код, построив цепочку прототипов так, чтобы
+// объект ancestor был прототипом для parent, а тот в свою очередь был прототипом для child.
+// Тесты
+// Объявлена переменная ancestor.
+// Значение переменной ancestor это объект.
+// Объявлена переменная parent.
+// Значение переменной parent это объект.
+// Объявлена переменная child.
+// Значение переменной child это объект.
+// Вызов parent.isPrototypeOf(child) возвращает true.
+// Обращение к parent.surname возвращает 'Moore'.
+// Обращение к parent.heritage возвращает 'Irish'.
+// Вызов parent.hasOwnProperty('surname') возвращает true.
+// Вызов parent.hasOwnProperty('heritage') возвращает false.
+// Вызов ancestor.isPrototypeOf(parent) возвращает true.
+// Вызов child.hasOwnProperty('surname') возвращает false.
+// Обращение к child.surname возвращает 'Moore'.
+// Обращение к child.heritage возвращает 'Irish'.
+// Вызов child.hasOwnProperty('heritage') возвращает false.
+// Обращение к ancestor.heritage возвращает 'Irish'.
+// Вызов ancestor.hasOwnProperty('surname') возвращает true.
+// Вызов ancestor.hasOwnProperty('heritage') возвращает true.
+// Обращение к ancestor.surname возвращает 'Dawson'.
+// Используется метод Object.create().
+
+const ancestor = {
+   name: 'Paul',
+   age: 83,
+   surname: 'Dawson',
+   heritage: 'Irish'
+ };
+ // Пиши код ниже этой строки
+
+ const parent = Object.create(ancestor); //const parent = {} - было изначально
+ parent.name = 'Stacey';
+ parent.surname = 'Moore';
+ parent.age = 54;
+
+ const child = Object.create(parent); //const child = {} - было изначально
+ child.name = 'Jason';
+child.age = 27;
+ 
+// Задание 3
+// Объяви функцию-конструктор Car которая принимает три параметра:
+
+// brand - марка автомобиля.
+// model - модель автомобиля.
+// price - цена автомобиля.
+// Функция Car должна создавать объект
+// с одноимёнными свойствами brand, model и price,
+//   значениями которых должны быть переданные аргументы во время её вызова с оператором new.
+
+// Тесты
+// Объявлена функция Car(brand, model, price).
+// В результате вызова new Car('Audi', 'Q3', 36000) получится объект
+// { brand: 'Audi', model: 'Q3', price: 36000 }.
+// В результате вызова new Car('BMW', 'X5', 58900) получится объект
+// { brand: 'BMW', model: 'X5', price: 58900 }.
+// В результате вызова new Car('Nissan', 'Murano', 31700) получится объект
+// { brand: 'Nissan', model: 'Murano', price: 31700 }.
+function Car(brand, model, price) {
+  this.brand = brand;
+  this.model = model;
+  this.price = price;
+}
+
+// Задание 4
+// Выполни рефакторинг функции - конструктора Car так, чтобы она принимала один параметр
+//   - объект со свойсвами brand, model и price.Деструктуризируй
+// объект в сигнатуре(подписи) функции.
+
+// Тесты
+// Объявлена функция Car({ brand, model, price }).
+// В результате вызова new Car({
+//   brand: 'Audi', model:
+//     'Q3', price: 36000
+// }) получится объект { brand: 'Audi', model: 'Q3', price: 36000 }.
+
+// В результате вызова new Car({ brand: 'BMW', model: 'X5', price: 58900 })
+// получится объект { brand: 'BMW', model: 'X5', price: 58900 }.
+// В результате вызова new Car({ brand: 'Nissan', model: 'Murano', price: 31700 })
+// получится объект { brand: 'Nissan', model: 'Murano', price: 31700 }.
+
+function Car({brand, model, price}) {
+  this.brand = brand;
+  this.model = model;
+  this.price = price;
+}
+// Задание 5
+// Добавь в свойство prototype функции-конструктора Car два метода:
+
+// getPrice() - возвращает значение свойства price из объекта который его будет вызывать.
+//   changePrice(newPrice) - обновляет значение свойства price у объекта который его будет
+// вызывать на newPrice.
+// Тесты
+// Объявлена функция Car({ brand, model, price }).
+// Вызов Car.prototype.hasOwnProperty('getPrice') возвращает true.
+// Значение Car.prototype.getPrice это функция.
+// Вызов Car.prototype.hasOwnProperty('changePrice') возвращает true.
+// Значение Car.prototype.changePrice это функция.
+// У объекта, созданного вызовом new Car({ brand: 'Audi', model: 'Q3', price: 36000 }),
+//   вызов метода getPrice() вернет число 36000.
+// У объекта, созданного вызовом new Car({ brand: 'Audi', model: 'Q3', price: 36000 }),
+//   вызов метода changePrice(35000) и последующем вызове getPrice() вернет число 35000.
+function Car({ brand, model, price }) {
+  this.brand = brand;
+  this.model = model;
+  this.price = price;
+}
+Car.prototype.getPrice = function () {
+  return this.price;
+};
+
+Car.prototype.changePrice = function (newPrice) {
+  this.price = newPrice;
+};
+
+// Задание 6
+// С помощью Function Declaration напиши функцию - конструктор Storage,
+//   которая будет создавать объекты для управления складом товаров.
+// Функция ожидает только один аргумент - начальный массив товаров, который
+// записывается на создаваемый объект в свойство items.
+
+// Добавь методы на прототип:
+
+// getItems() - возвращает массив текущих товаров в свойстве items
+// объекта, который вызывает этот метод.
+//   addItem(newItem) - принимает новый товар newItem и добавляет
+// его в массив товаров в свойстве items объекта, который вызывает этот метод.
+//   removeItem(item) - принимает товар item и удаляет его из массива
+// товаров в свойстве items объекта, который вызывает этот метод.
+// Под комментарием мы добавили инициализацию экземпляра и вызовы
+// методов в той последовательности, в которой твой код будут проверять тесты.
+// Пожалуйста ничего там не меняй.
+
+// Тесты
+// Объявлена функция Storage(items).
+// Вызов Storage.prototype.hasOwnProperty('getItems') возвращает true.
+// Вызов Storage.prototype.hasOwnProperty('addItem') возвращает true.
+// Вызов Storage.prototype.hasOwnProperty('removeItem') возвращает true.
+// В результате вызова new Storage(['Нанитоиды', 'Пролонгер', 'Антигравитатор'])
+// значение переменной storage это объект.
+// Вызов Storage.prototype.isPrototypeOf(storage) возвращает true.
+// У объекта storage есть свойство items.
+// Первый вызов storage.getItems(), сразу после инциализации экземпляра, возвращает массив
+// ["Нанитоиды", "Пролонгер", "Антигравитатор"].
+// Второй вызов, storage.getItems(), после вызова storage.addItem('Дроид'), возвращает массив
+// ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"].
+// Третий вызов storage.getItems(), после вызова storage.removeItem('Пролонгер'),
+//   возвращает массив["Нанитоиды", "Антигравитатор", "Дроид"].
+function Storage(items) {
+     this.items = items;
+ };
+
+ Storage.prototype.getItems = function () {
+     return this.items;
+ };
+
+ Storage.prototype.addItem = function (newItem) {
+     this.items.push(newItem);
+ };
+
+ Storage.prototype.removeItem = function (removeItem) {
+     for (let i = 0; i < this.items.length; i++) {
+         if (this.items.indexOf(removeItem) === i) {
+             this.items.splice(i, 1)
+         };
+     };
+ };
+// // Пиши код выше этой строки
+ const storage = new Storage(['Нанитоиды', 'Пролонгер', 'Антигравитатор']);
+ console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]/ storage.addItem('Дроид');
+ console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+ storage.removeItem('Пролонгер');
+ console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+
+

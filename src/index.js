b19636645cf231a1f4d846e0c103173c98243ac7
OleksiiHordiienko82 <79364122+OleksiiHordiienko82@users.noreply.github.com>
2021-06-22 12:51:42 +0300

    mod 6 task 30-40

--- src/index.js
+++ src/index.js
@@ -4727,3 +4727,270 @@ return time + player;
 
 // Пиши код выше этой строки
 const averagePlayTime = totalPlayTime / playtimes.length;
+
+// Задание 31
+// Нашему сервису необходимо рассчитать среднее время
+// проведённое в одной игре для каждого игрока,
+//   и получить общую сумму этих времён.
+// Рассчитать время для каждого из игроков,
+//   можно разделив его время(свойство playtime) на количество игр(свойство gamesPlayed).
+
+// Тесты
+// Объявлена переменная players.
+// Значение переменной players это массив объектов игроков.
+// Объявлена переменная totalAveragePlaytimePerGame.
+// Значение переменной totalAveragePlaytimePerGame это число 1023.
+// Для перебора массива players используется метод reduce().
+const players = [
+  { name: 'Манго', playtime: 1270, gamesPlayed: 4 },
+  { name: 'Поли', playtime: 469, gamesPlayed: 2 },
+  { name: 'Аякс', playtime: 690, gamesPlayed: 3 },
+  { name: 'Киви', playtime: 241, gamesPlayed: 1 },
+];
+// Пиши код ниже этой строки
+
+const totalAveragePlaytimePerGame =  players.reduce((total, player) => {
+  return total + player.playtime / player.gamesPlayed ;
+}, 0);
+
+// Задание 32
+// Дополни функцию calculateTotalBalance(users) так,
+//   чтобы она считала и возвращала сумму всех средств(свойство balance)
+// которые хранят пользователи из массива users.
+
+// Тесты
+// Объявлена переменная calculateTotalBalance.
+// Переменной calculateTotalBalance присвоена стрелочная функция с параметром (users).
+// Для перебора параметра users используется метод reduce().
+// Вызов функции с указанным массивом пользователей возвращает число 20916.
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение.
+// Пиши код ниже этой строки
+const calculateTotalBalance = users => {
+ const totalBalance = users.reduce(function(balance,user){
+return balance + user.balance;
+},0);
+  return totalBalance;
+};
+// Пиши код выше этой строки 
+// Задание 33
+// Дополни функцию getTotalFriendCount(users) так, 
+//чтобы она считала и возвращала общее количество друзей
+  //(свойство friends) всех пользователей из массива users.
+
+// Тесты
+// Объявлена переменная getTotalFriendCount.
+// Переменной getTotalFriendCount присвоена стрелочная функция с параметром (users).
+// Для перебора параметра users используется метод reduce().
+// Вызов функции с указанным массивом пользователей возвращает число 14
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение.
+// Пиши код ниже этой строки
+const getTotalFriendCount = users => {
+    return users.reduce(function(total,user){
+      return total + user.friends.length
+  },0) 
+};
+// Пиши код выше этой строки
+
+// Задание 34
+// Дополни код так, чтобы в переменной ascendingReleaseDates
+// получилась отсортированная по возрастанию копия массива releaseDates,
+//   а в переменной alphabeticalAuthors копия массива имён авторов authors отсортированная в по алфавиту.
+// Тесты
+// Объявлена переменная releaseDates.
+// Значение переменной releaseDates это массив [2016, 1967, 2008, 1984, 1973, 2012, 1997].
+// Объявлена переменная authors.
+// Значение переменной authors это массив 
+// ['Ли Танит', 'Бернард Корнуэлл', 'Роберт Шекли', 'Федор Достоевский'].
+// Объявлена переменная ascendingReleaseDates.
+// Значение переменной ascendingReleaseDates это массив
+// [1967, 1973, 1984, 1997, 2008, 2012, 2016].
+// Объявлена переменная alphabeticalAuthors.
+// Значение переменной alphabeticalAuthors это массив
+// ['Бернард Корнуэлл', 'Ли Танит', 'Роберт Шекли', 'Федор Достоевский'].
+// Использован метод sort().
+const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+const authors = [
+  'Ли Танит',
+  'Бернард Корнуэлл',
+  'Роберт Шекли',
+  'Федор Достоевский'
+];
+// Пиши код ниже этой строки
+
+const ascendingReleaseDates = [...releaseDates].sort();
+
+const alphabeticalAuthors = [...authors].sort();
+
+// Задание 35
+// Онлайн бибилиотеке необходимо отображать книги сортированные по дате издания,
+//   по её возрастанию или убыванию.Дополни код так,
+//   чтобы в переменной ascendingReleaseDates
+// получилась отсортированная по возрастанию копия массива releaseDates,
+//   а в переменной descendingReleaseDates копия отсортированная по убыванию.
+
+// Тесты
+// Объявлена переменная releaseDates.
+// Значение переменной releaseDates это массив [2016, 1967, 2008, 1984, 1973, 2012, 1997].
+// Объявлена переменная ascendingReleaseDates.
+// Значение переменной ascendingReleaseDates это массив [1967, 1973, 1984, 1997, 2008, 2012, 2016].
+// Объявлена переменная descendingReleaseDates.
+// Значение переменной descendingReleaseDates это массив [2016, 2012, 2008, 1997, 1984, 1973, 1967].
+// Использован метод sort().
+const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// Пиши код ниже этой строки
+
+const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b );
+
+const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
+
+// Задание 36
+// Онлайн бибилиотеке необходимо отображать книги отсортированные по автору,
+//   в алфавитном и обратном алфавитном порядке.Дополни код так,
+//   чтобы в переменной authorsInAlphabetOrder получилась отсортированная
+// по алфавиту копия массива authors, а в переменной authorsInReversedOrder копия
+// отсортированная в обратном алфавитном порядке.
+
+// Тесты
+// Объявлена переменная authors.
+// Значение переменной authors это массив
+// ['Ли Танит', 'Бернард Корнуэлл', 'Роберт Шекли', 'Федор Достоевский', 'Говард Лавкрафт'].
+// Объявлена переменная authorsInAlphabetOrder.
+// Значение переменной authorsInAlphabetOrder это массив
+// ['Бернард Корнуэлл', 'Говард Лавкрафт', 'Ли Танит', 'Роберт Шекли', 'Федор Достоевский'].
+// Объявлена переменная authorsInReversedOrder.
+// Значение переменной authorsInReversedOrder это массив
+// ['Федор Достоевский', 'Роберт Шекли', 'Ли Танит', 'Говард Лавкрафт', 'Бернард Корнуэлл'].
+// Использован метод sort().
+
+const authors = [
+    'Ли Танит',
+    'Бернард Корнуэлл',
+    'Роберт Шекли',
+    'Федор Достоевский',
+    'Говард Лавкрафт'
+  ];
+  // Пиши код ниже этой строки
+
+  const authorsInAlphabetOrder = [...authors].sort((a, b) => a.localeCompare(b));
+
+const authorsInReversedOrder = [...authors].sort((a, b) => b.localeCompare(a));
+  
+// Задание 37
+// Дополни код так, чтобы:
+
+// В переменной sortedByAuthorName получился массив книг
+// отсортированный по имени автора в алфавитном порядке.
+// В переменной sortedByReversedAuthorName получился массив книг
+// отсортированный по имени автора в обратном алфавитном порядке.
+// В переменной sortedByAscendingRating получился массив книг
+// отсортированный по возрастанию рейтинга.
+// В переменной sortedByDescentingRating получился массив книг
+// отсортированный по убыванию рейтинга.
+// Тесты
+// Объявлена переменная books.
+// Значение переменной books это исходный массив объектов книг.
+// Объявлена переменная sortedByAuthorName.
+// Значение переменной sortedByAuthorName это массив книг
+// отсортированный по имени автора в алфавитном порядке.
+// Объявлена переменная sortedByReversedAuthorName.
+// Значение переменной sortedByReversedAuthorName это массив
+// книг отсортированный по имени автора в обратном алфавитном порядке.
+// Объявлена переменная sortedByAscendingRating.
+// Значение переменной sortedByAscendingRating это массив
+// книг отсортированный по возрастанию рейтинга.
+// Объявлена переменная sortedByDescentingRating.
+// Значение переменной sortedByDescentingRating это массив
+// книг отсортированный по убыванию рейтинга.
+// Для перебора массива books используется метод sort().
+const books = [
+  { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+  { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+  { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+  { title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
+  { title: 'Враг Божий', author: 'Бернард Корнуэлл', rating: 8.67 }
+];
+// Пиши код ниже этой строки
+
+const sortedByAuthorName = [...books].sort(
+  (firstBook, secondBook) => firstBook.author.localeCompare(secondBook.author) 
+);
+
+const sortedByReversedAuthorName = [...books].sort(
+  (firstBook, secondBook) =>  secondBook.author.localeCompare(firstBook.author) 
+);
+
+const sortedByAscendingRating = [...books].sort(
+  (firstBook, secondBook) => firstBook.rating - secondBook.rating
+);
+const sortedByDescentingRating = [...books].sort(
+  (firstBook, secondBook) => secondBook.rating - firstBook.rating
+);
+
+// Задание 38
+// Дополни функцию sortByAscendingBalance(users) так,
+//   чтобы она возвращала массив пользователей отсортированный
+// по возрастанию их баланса(свойство balance).
+
+// Тесты
+// Объявлена переменная sortByAscendingBalance.
+// Переменной sortByAscendingBalance присвоена стрелочная
+// функция с параметром(users).
+// Значение параметра users не изменяется.
+// Вызов функции с указанным массивом пользователей возвращает
+// новый массив пользователей отсортированный по возрастанию их баланса.
+// Вызов функции со случайными, но валидными аргументами,
+//   возвращает правильное значение.
+// Для перебора параметра users использован метод sort().
+// Пиши код ниже этой строки
+const sortByAscendingBalance = users => {
+ const ascendingBalance = users.sort(function(prevBalance, nextBalance){
+  return prevBalance.balance - nextBalance.balance;
+   })
+ return ascendingBalance;
+};
+// Пиши код выше этой строки
+
+// Задание 39
+// Дополни функцию sortByDescendingFriendCount(users) так,
+//   чтобы она возвращала массив пользователей отсортированный
+// по убыванию количества их друзей(свойство friends).
+
+// Тесты
+// Объявлена переменная sortByDescendingFriendCount.
+// Переменной sortByDescendingFriendCount присвоена стрелочная
+// функция с параметром(users).
+// Значение параметра users не изменяется.
+// Для перебора параметра users использован метод sort().
+// Вызов функции с указанным массивом пользователей возвращает новый
+// массив пользователей отсортированный по убыванию количества их друзей.
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение.
+// Пиши код ниже этой строки
+const sortByDescendingFriendCount = users => {
+  const friendCount = [...users].sort(function(prevCount, nextCount){
+  return nextCount.friends.length - prevCount.friends.length;
+   })
+ return friendCount; 
+};
+// Пиши код выше этой строки
+
+// Задание 40
+// Дополни функцию sortByName(users) так, чтобы она возвращала
+// массив пользователей отсортированный по их имени(свойство name) в алфавитном порядке.
+
+// Тесты
+// Объявлена переменная sortByName.
+// Переменной sortByName присвоена стрелочная функция с параметром (users).
+// Значение параметра users не изменяется.
+// Для перебора параметра users использован метод sort().
+// Вызов функции с указанным массивом пользователей возвращает новый
+// массив пользователей отсортированный по имени в алфавитном порядке.
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение.
+
+// Пиши код ниже этой строки
+const sortByName = users => {
+   const byName = [...users].sort(function(prevName, nextName){
+  return prevName.name.localeCompare(nextName.name)
+   })
+ return byName; 
+};
+// Пиши код выше этой строки
